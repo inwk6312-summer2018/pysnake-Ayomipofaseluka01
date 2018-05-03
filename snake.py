96ecb22b9c888931b29b8186ef5786af569dd850
ayomipo <Ayomipo.faseluka@dal.ca>
2018-05-03 11:13:12 -0300

    first commit

---
+++ snake.py
@@ -0,0 +1,7 @@
+1-create a moving object
+2-create a feed
+3-create a restriction
+4-for movement in object . append feed
+5-if object is appended, append feed
+6-if object is appended, increase in size
+7-for every movement in object ,if restriction is true, crash 
